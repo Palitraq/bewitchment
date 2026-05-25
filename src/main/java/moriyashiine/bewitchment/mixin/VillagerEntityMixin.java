31933596146baad8ec03bd9643f184c52fb1507d
Palitraq <seryhsdr2@gmail.com>
2026-05-25 18:10:56 +0300

    fix villager crash ClassCastException

--- src/main/java/moriyashiine/bewitchment/mixin/VillagerEntityMixin.java
+++ src/main/java/moriyashiine/bewitchment/mixin/VillagerEntityMixin.java
@@ -5,13 +5,11 @@
 package moriyashiine.bewitchment.mixin;
 
 import moriyashiine.bewitchment.common.misc.BWUtil;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.village.TradeOfferList;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,19 +17,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(VillagerEntity.class)
-public abstract class VillagerEntityMixin extends MerchantEntity {
+public abstract class VillagerEntityMixin {
 	@Shadow
 	protected abstract void sayNo();
 
-	public VillagerEntityMixin(EntityType<? extends MerchantEntity> entityType, World world) {
-		super(entityType, world);
-	}
-
 	@Inject(method = "interactMob", at = @At(value = "INVOKE", shift = At.Shift.BEFORE, target = "Lnet/minecraft/entity/passive/VillagerEntity;getOffers()Lnet/minecraft/village/TradeOfferList;"), cancellable = true)
-	private void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<TradeOfferList> callbackInfo) {
-		if (!getWorld().isClient && BWUtil.rejectTrades(this)) {
+	private void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> callbackInfo) {
+		LivingEntity merchant = (LivingEntity) (Object) this;
+		if (!merchant.getWorld().isClient && BWUtil.rejectTrades(merchant)) {
 			sayNo();
-			callbackInfo.setReturnValue(BWUtil.EMPTY_TRADES);
+			callbackInfo.setReturnValue(ActionResult.success(false));
 		}
 	}
 }
